bcb1d40d3d8827d06c70f548e98e4c06fa35a247
Fredy <desaMts1806@MacBook-Pro-de-macpro.local>
2019-10-22 16:23:27 -0500

    Configuracion de Webpack con React listo para comenzar a desarrollar

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hola React!</h1>
+                <p>Aqui es un parrafo</p> 
+            </div>  
+        );
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/**
+ * Configuracion de Webpack
+*/
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry:'./src/index.js',
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename:'app.build.js'
+    },
+    module:{
+        rules:[{
+            test: /\.js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        }]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      })
+    ]
+}
